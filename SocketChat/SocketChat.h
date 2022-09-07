0982157242b1c7146029631c4a060022d36a0550
Leo <agskishor@gmail.com>
2022-09-07 12:25:50 +0530

    Initial Commit

---
+++ SocketChat/SocketChat.h
@@ -0,0 +1,18 @@
+//
+//  SocketChat.h
+//  SocketChat
+//
+//  Created by MAcBook on 07/09/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SocketChat.
+FOUNDATION_EXPORT double SocketChatVersionNumber;
+
+//! Project version string for SocketChat.
+FOUNDATION_EXPORT const unsigned char SocketChatVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SocketChat/PublicHeader.h>
+
+
